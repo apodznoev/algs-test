31bb776bb77dff431b98b60b61eeb7c073c56355
apodznoev <a.podznoev@reply.de>
2020-02-25 18:47:05 +0100

    Majority & single element

---
+++ src/de/avpod/SingleNumber.java
@@ -0,0 +1,21 @@
+package de.avpod;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SingleNumber {
+
+    public static void main(String[] args) {
+        System.out.println(singleNumber(Arrays.asList(
+                1, 2, 3, 4, 4, 5, 3, 2, 1
+        )));
+    }
+
+    public static int singleNumber(final List<Integer> A) {
+        int result = 0;
+        for(int i : A) {
+            result = result ^ i;
+        }
+        return result;
+    }
+}
