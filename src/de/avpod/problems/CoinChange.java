1ee475d2f0514852262205a27f847aae0fbde24e
Andrei Podznoev <andrei.podznoev@enmacc.com>
2022-05-01 12:54:39 +0200

    CoinChange problem (not optimal)

---
+++ src/de/avpod/problems/CoinChange.java
@@ -0,0 +1,54 @@
+package de.avpod.problems;
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+    public static void main(String[] args) {
+        CoinChange coinChange = new CoinChange();
+        int[] coins = Arrays.stream(args[0].split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int amount = Integer.valueOf(args[1]);
+        System.out.print(Arrays.toString(coins));
+        System.out.print(",");
+        System.out.println(amount);
+        System.out.println(coinChange.coinChange(coins, amount));
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        Arrays.sort(coins);
+
+        return greedyChange(coins, amount);
+    }
+
+    private int greedyChange(int[] coins, int amount) {
+        int coinsLength = coins.length;
+        int largestCoinValue = coins[coinsLength - 1];
+        int largestCoinMaxCount = amount / largestCoinValue;
+
+        if (amount % largestCoinValue == 0) {
+            return largestCoinMaxCount;
+        }
+
+        if (coins.length == 1) {
+            return -1;
+        }
+
+        for (int currentCoinsCount = largestCoinMaxCount; currentCoinsCount >= 0; currentCoinsCount--) {
+            int remainingAmount = amount - largestCoinValue * currentCoinsCount;
+            int[] remainingCoins = Arrays.copyOf(coins, coinsLength - 1);
+            int solvedCoinsCount = greedyChange(remainingCoins, remainingAmount);
+
+            if (solvedCoinsCount != -1) {
+                return currentCoinsCount + solvedCoinsCount;
+            }
+        }
+
+        return -1;
+    }
+}
