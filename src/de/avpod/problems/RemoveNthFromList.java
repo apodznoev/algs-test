3f7a52fd7cb14ce053386b1cefdecc6c731302e8
Andrei Podznoev <andrei.podznoev@enmacc.com>
2022-05-07 13:25:58 +0200

    Remove Nth from end linked list

--- src/de/avpod/problems/RemoveNthFromList.java
+++ src/de/avpod/problems/RemoveNthFromList.java
@@ -11,21 +11,33 @@ public class RemoveNthFromList {
         System.out.println(nth.removeNthFromStart(head, 2));
     }
 
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tmp = head;
+        int length = 0;
+        while (tmp != null) {
+            tmp = tmp.next;
+            length++;
+        }
+
+        return removeNthFromStart(head, length - n);
+    }
+
     public ListNode removeNthFromStart(ListNode head, int n) {
+        if(n == 0) {
+            return head.next;
+        }
+
         ListNode result = head;
         ListNode nthPrev = null;
         ListNode nth = head;
         ListNode nthNext = head.next;
 
-        ListNode tmp = null;
-
         int i = 0;
         while (i++ < n) {
-            tmp = nthNext;
 
             nthPrev = nth;
             nth = nthNext;
-            nthNext = tmp.next;
+            nthNext = nthNext.next;
         }
 
         nthPrev.next = nthNext;
