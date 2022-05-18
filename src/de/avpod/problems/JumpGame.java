fe40cd2281c46d41cb03fd30a4c1fb52c6ccc7ab
Andrei Podznoev <andrei.podznoev@enmacc.com>
2022-05-18 08:21:38 +0200

    JumpGame solution

---
+++ src/de/avpod/problems/JumpGame.java
@@ -0,0 +1,37 @@
+package de.avpod.problems;
+
+import de.avpod.util.ConsoleUtil;
+
+public class JumpGame {
+    public static void main(String[] args) {
+        JumpGame jumpGame = new JumpGame();
+        System.out.println(jumpGame.canJump(ConsoleUtil.firstArgToIntArrayWithBrackets(args)));
+    }
+    public boolean canJump(int[] nums) {
+        return solvePuzzle(nums, new boolean[nums.length], 0);
+    }
+
+    private boolean solvePuzzle(final int[] nums, final boolean[] visited, final int index) {
+        if (nums.length - 1 == index) {
+            return true;
+        }
+
+        if (index >= nums.length) {
+            return false;
+        }
+
+        if (visited[index]) {
+            return false;
+        }
+
+        int jumps = nums[index];
+        for (int i = jumps; i > 0; i--) {
+            if (solvePuzzle(nums, visited, index + i)) {
+                return true;
+            }
+        }
+        visited[index] = true;
+
+        return false;
+    }
+}
