e75f16d8f754398f10157a433425d4d8260e44de
Andrei Podznoev <andrei.podznoev@enmacc.com>
2022-06-26 12:56:05 +0200

    Best sightseeing pair solution

---
+++ src/de/avpod/problems/BestSighseeingPair.java
@@ -0,0 +1,49 @@
+package de.avpod.problems;
+
+public class BestSighseeingPair {
+    int[] spots;
+
+
+//    [2,7,7,2,1,7,10,4,3,3]
+
+    public int maxScoreSightseeingPair(int[] values) {
+        this.spots = values;
+        int score = score(0, 1);
+        return findMax(score, 0, 1, 1, 2);
+    }
+
+    private int findMax(final int curScore,
+                        final int first,
+                        final int second,
+                        final int prevSpot,
+                        final int currentSpot) {
+        if (currentSpot == spots.length) {
+            return curScore;
+        }
+
+        int scoreFirstCurrent = score(first, currentSpot);
+        int scoreSecondCurrent = score(second, currentSpot);
+        int scorePrevCurrent = score(prevSpot, currentSpot);
+        if (curScore > scoreFirstCurrent && curScore > scoreSecondCurrent && curScore > scorePrevCurrent) {
+            if(spots[currentSpot] + Math.abs(currentSpot - prevSpot) > spots[prevSpot]) {
+                return findMax(curScore, first, second, currentSpot, currentSpot + 1);
+            } else {
+                return findMax(curScore, first, second, prevSpot, currentSpot + 1);
+            }
+        }
+
+        if (scoreFirstCurrent > scoreSecondCurrent && scoreFirstCurrent > scorePrevCurrent) {
+            return findMax(scoreFirstCurrent, first, currentSpot, currentSpot, currentSpot + 1);
+        }
+
+        if (scoreSecondCurrent > scoreFirstCurrent && scoreSecondCurrent > scorePrevCurrent) {
+            return findMax(scoreSecondCurrent, second, currentSpot, currentSpot, currentSpot + 1);
+        }
+
+        return findMax(scorePrevCurrent, prevSpot, currentSpot, currentSpot, currentSpot + 1);
+    }
+
+    private int score(final int i, final int j) {
+        return spots[i] + spots[j] - Math.abs(i - j);
+    }
+}
